a728f7f4a83a48c064dc3a09173d19b5a67f6d9a
Michał Tracichleb <111907641+michal-tracichleb@users.noreply.github.com>
2023-10-25 19:23:49 +0200

    Handling CRUD in API Controller for PartTransactions and OperationalDocuments

---
+++ QuirkyCarRepairApi/QuirkyCarRepair.API/Controllers/Warehouse/OperationalDocumentController.cs
@@ -0,0 +1,61 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using QuirkyCarRepair.API.DTO.Warehouse;
+using QuirkyCarRepair.BLL.Areas.Warehouse.Entities;
+using QuirkyCarRepair.BLL.Areas.Warehouse.Interfaces;
+
+namespace QuirkyCarRepair.API.Controllers.Warehouse
+{
+    [Route("api/Warehouse/[controller]")]
+    [ApiController]
+    public class OperationalDocumentController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly IOperationalDocumentService _operationalDocumentService;
+
+        public OperationalDocumentController(IMapper mapper,
+            IOperationalDocumentService operationalDocumentService)
+        {
+            _mapper = mapper;
+            _operationalDocumentService = operationalDocumentService;
+        }
+
+        // GET: api/<OperationalDocumentController>
+        [HttpGet]
+        public IEnumerable<OperationalDocumentDTO> Get()
+        {
+            return _mapper.Map<List<OperationalDocumentDTO>>(_operationalDocumentService.GetAll());
+        }
+
+        // GET api/<OperationalDocumentController>/5
+        [HttpGet("{id}")]
+        public OperationalDocumentDTO Get(int id)
+        {
+            return _mapper.Map<OperationalDocumentDTO>(_operationalDocumentService.Get(id));
+        }
+
+        // POST api/<OperationalDocumentController>
+        [HttpPost]
+        public ActionResult<OperationalDocumentDTO> Post([FromBody] OperationalDocumentDTO model)
+        {
+            var operationalDocumentEntity = _mapper.Map<OperationalDocumentEntity>(model);
+            var newOperationalDocument = _mapper.Map<OperationalDocumentDTO>(_operationalDocumentService.Creat(operationalDocumentEntity));
+
+            return CreatedAtAction(nameof(Get), newOperationalDocument.Id, newOperationalDocument);
+        }
+
+        // PUT api/<OperationalDocumentController>/5
+        [HttpPut("{id}")]
+        public void Update(int id, [FromBody] OperationalDocumentDTO model)
+        {
+            _operationalDocumentService.Update(id, _mapper.Map<OperationalDocumentEntity>(model));
+        }
+
+        // DELETE api/<OperationalDocumentController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _operationalDocumentService.Delete(id);
+        }
+    }
+}
---
+++ QuirkyCarRepairApi/QuirkyCarRepair.API/DTO/Warehouse/PartTransactionDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace QuirkyCarRepair.API.DTO.Warehouse
+{
+    public class PartTransactionDTO
+    {
+        public int Id { get; set; }
+        public int PartId { get; set; }
+        public int OperationalDocumentId { get; set; }
+
+        public decimal Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal MarginValue { get; set; }
+    }
+}
