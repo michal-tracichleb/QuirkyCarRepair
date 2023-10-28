318b47f2e3e947511bedac317cde99367de17206
Michał Tracichleb <111907641+michal-tracichleb@users.noreply.github.com>
2023-10-28 12:52:36 +0200

    Added part properties - Manufacturer, Model, ProductCode, CountryOfOrigin, Weight, Height, Width, Depth

--- QuirkyCarRepairApi/QuirkyCarRepair.DAL/Areas/Warehouse/Models/Part.cs
+++ QuirkyCarRepairApi/QuirkyCarRepair.DAL/Areas/Warehouse/Models/Part.cs
@@ -20,6 +20,15 @@ namespace QuirkyCarRepair.DAL.Areas.Warehouse.Models
         public string UnitType { get; set; }
         public decimal UnitPrice { get; set; }
 
+        public string? Manufacturer { get; set; }
+        public string? Model { get; set; }
+        public string? ProductCode { get; set; }
+        public string? CountryOfOrigin { get; set; }
+        public double? Weight { get; set; }
+        public double? Height { get; set; }
+        public double? Width { get; set; }
+        public double? Depth { get; set; }
+
         public virtual PartCategory PartCategory { get; set; }
         public virtual Margin Margin { get; set; }
         public virtual ICollection<PartTransaction> PartTransactions { get; set; }
