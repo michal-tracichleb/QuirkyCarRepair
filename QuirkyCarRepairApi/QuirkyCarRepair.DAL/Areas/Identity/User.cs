3e9e9d3754878b10406d1d34787259982189e308
Michał Tracichleb <111907641+michal-tracichleb@users.noreply.github.com>
2023-11-03 19:16:26 +0100

    Added model for TransactionStatus

--- QuirkyCarRepairApi/QuirkyCarRepair.DAL/Areas/Identity/User.cs
+++ QuirkyCarRepairApi/QuirkyCarRepair.DAL/Areas/Identity/User.cs
@@ -10,14 +10,18 @@ namespace QuirkyCarRepair.DAL.Areas.Identity
         public User() : base()
         {
             OperationalDocuments = new HashSet<OperationalDocument>();
+            TransactionStatus = new HashSet<TransactionStatus>();
+
+            ServiceOrders = new HashSet<ServiceOrder>();
             ServiceOrderStatuses = new HashSet<ServiceOrderStatus>();
             Vehicles = new HashSet<Vehicle>();
-            ServiceOrders = new HashSet<ServiceOrder>();
         }
 
         public virtual ICollection<OperationalDocument> OperationalDocuments { get; set; }
+        public virtual ICollection<TransactionStatus> TransactionStatus { get; set; }
+
+        public virtual ICollection<ServiceOrder> ServiceOrders { get; set; }
         public virtual ICollection<ServiceOrderStatus> ServiceOrderStatuses { get; set; }
         public virtual ICollection<Vehicle> Vehicles { get; set; }
-        public virtual ICollection<ServiceOrder> ServiceOrders { get; set; }
     }
 }
