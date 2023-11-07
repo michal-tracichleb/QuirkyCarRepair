380cd36f81265ec40724fb2f000e300148db9101
Michał Tracichleb <111907641+michal-tracichleb@users.noreply.github.com>
2023-11-07 22:49:42 +0100

    Removed redundant file with Validation. Removed property 'RoleId' from Dto.

--- QuirkyCarRepairApi/QuirkyCarRepair.BLL/Areas/Identity/DTO/RegisterUserDto.cs
+++ QuirkyCarRepairApi/QuirkyCarRepair.BLL/Areas/Identity/DTO/RegisterUserDto.cs
@@ -10,7 +10,5 @@
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
         public string? PhoneNumber { get; set; }
-
-        public int RoleId { get; set; } = 1;
     }
 }
