72f37b741405baa8029675b9a9503888bb1db98e
Oskar <oskar0201@onet.pl>
2023-12-03 20:01:50 +0100

    added styles for component

--- quirky-car-repair/src/components/Alert/Alert.jsx
+++ quirky-car-repair/src/components/Alert/Alert.jsx
@@ -1,6 +1,7 @@
+import styles from "./Alert.module.css"
 export function Alert({children, color = "primary"}){
     return(
-        <div className={`alert alert-${color}`} role="alert">
+        <div className={`alert alert-${color} ${styles.myalert}`} role="alert">
             <span>{children}</span>
         </div>
     )
