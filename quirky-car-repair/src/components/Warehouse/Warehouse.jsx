994b03f397e48ff8815ca9b0cf9f07ad84e5466c
Oskar <oskar0201@onet.pl>
2023-12-20 17:09:49 +0100

    deleted todo from file

--- quirky-car-repair/src/components/Warehouse/Warehouse.jsx
+++ quirky-car-repair/src/components/Warehouse/Warehouse.jsx
@@ -1,11 +1,6 @@
 import {Layout} from "../Layout/Layout.jsx";
 import {Outlet} from "react-router-dom";
 
-/*TODO:
-* Dodanie wybrania ilości produktów na stronie, dodanie przełącznika stron produktów
-* Stworzyć funkcję wyświetlającą ścieżkę kategorii (po edycji endpointa)
-*/
-
 export function Warehouse() {
     return (
         <>
