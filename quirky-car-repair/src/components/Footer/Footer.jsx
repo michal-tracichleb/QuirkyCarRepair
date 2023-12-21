11cee654e0363a2cb456940499f4fd2f2ad47791
Oskar <oskar0201@onet.pl>
2023-12-22 00:19:02 +0100

    Stopka strony

---
+++ quirky-car-repair/src/components/Footer/Footer.jsx
@@ -0,0 +1,8 @@
+import styles from "./Footer.module.css"
+export function Footer(){
+    return(
+        <div className={styles.footer}>
+            <p>QuirkyCarRepair 2023/2024</p>
+        </div>
+    )
+}
