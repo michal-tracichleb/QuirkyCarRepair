4e280f18b207a52f85516514ffdce7cfbfa076b7
Oskar <oskar0201@onet.pl>
2024-01-02 18:25:10 +0100

    nowy widok listy produktów + loader produktu

--- quirky-car-repair/src/components/Warehouse/ProductsList/ProductsList.jsx
+++ quirky-car-repair/src/components/Warehouse/ProductsList/ProductsList.jsx
@@ -1,44 +1,38 @@
 import styles from "./ProductsList.module.css";
 import {AddProductToCart} from "../../../utlis/AddProductToCart.js";
-import {useContext, useState} from "react";
-import {ProductModal} from "../ProductModal/ProductModal.jsx";
-import {AlertStateContext} from "../../../context/AlertStateContext.js";
+import {Link} from "react-router-dom";
 
 export function ProductsList({productsData}){
-const [product, setProduct] = useState({});
-const [alert, setAlert] = useContext(AlertStateContext);
 
-const productAddedAlert=()=>{
-        setAlert({color:'success', text: 'Dodano produkt do koszyka'})
-        setTimeout(() => {
-            setAlert();
-        }, 3000);
-}
-
-const handleSetProduct=(data)=>{
-    setProduct(data);
-}
     return(
-        <>
-            {productsData && productsData.map((product)=> (
-                <div className={`row ${styles.product}`} key={product.id}>
-                    <div className="col-8"  key={product.id} data-bs-toggle="modal" data-bs-target="#productModal" onClick={()=>handleSetProduct(product)}>
-                        <h3>{product.name}</h3>
-                        <p className={styles.manufacturer}>Producent: {product.manufacturer}</p>
-                        <p>Model: {product.model}</p>
-                        <p className={styles.code}>Kod produktu: {product.productCode}</p>
-                    </div>
-                    <div className="col-4 border-start border-1 text-center ">
-                        <h3 className={styles.price}>{product.unitPrice} zł /szt.</h3>
-                        <button className={styles.btn} onClick={()=> {
-                            AddProductToCart(product.id, product.name, product.unitPrice);
-                            productAddedAlert()
-                        }}>Dodaj do koszyka</button>
-                    </div>
+        <div className={styles.container}>
+            {productsData && productsData.map((product)=>(
+                <div className={styles.productContainer} key={product.id}>
+                        <div className={styles.product}>
+                            <Link to={`product/${product.id}`}>
+                                <div>
+                                    <h3>{product.name}</h3>
+                                    <p className={styles.manufacturer}>Producent: {product.manufacturer}</p>
+                                    <p>Model: {product.model}</p>
+                                    <p className={styles.code}>Kod produktu: {product.productCode}</p>
+                                    <div className={styles.quantity}>
+                                        <h3>Ilość: <span className={`${(product.quantity === 0) ? styles.dangerous : (product.quantity < product.minimumQuantity) ? styles.warning : styles.success}`}>{product.quantity}</span></h3>
+                                        {product.quantity === 0 &&
+                                            <h3 className={styles.dangerous}>Brak na stanie</h3>
+                                        }
+                                    </div>
+                                </div>
+                            </Link>
+                        </div>
+                        <div className={styles.toolbox}>
+                            <h3 className={styles.price}>{product.unitPrice} zł /szt.</h3>
+                            <button className={styles.btn} onClick={()=> {
+                                AddProductToCart(product.id, product.name, product.unitPrice);
+                            }}>Dodaj do koszyka</button>
+                        </div>
                 </div>
             ))}
-            <ProductModal productData={product} Alert={productAddedAlert}/>
-        </>
+        </div>
 
     )
 }
--- quirky-car-repair/src/components/Warehouse/ProductsPanel/ProductsPanel.jsx
+++ quirky-car-repair/src/components/Warehouse/ProductsPanel/ProductsPanel.jsx
@@ -1,29 +1,32 @@
-import {useParams} from "react-router-dom";
-import {useEffect, useState, useContext, useRef} from "react";
+import styles from "./ProductsPanel.module.css"
+import {Link, useParams, useSearchParams} from "react-router-dom";
+import {useEffect, useState, useRef} from "react";
 import axios from "axios";
-import {SideBar} from "../../SideBar/SideBar.jsx";
-import {CategoriesList} from "../CategoryList/CategoriesList.jsx";
-import {Header} from "../Header/Header.jsx";
 import {ProductsList} from "../ProductsList/ProductsList.jsx";
-import {AlertStateContext} from "../../../context/AlertStateContext.js";
 import {Pagination} from "../../Pagination/Pagination.jsx";
 import {PaginationSmall} from "../../PaginationSmall/PaginationSmall.jsx";
+import {Container} from "../../ContentLayout/Container/Container.jsx";
+import {Sidebar} from "../../ContentLayout/Sidebar/Sidebar.jsx";
+import {MainContent} from "../../ContentLayout/MainContent/MainContent.jsx";
+import {BACK_END_URL} from "../../../constans/backEndUrl.js";
 
 export function ProductsPanel (){
     const { categoryId } = useParams();
-    const { pageId } = useParams();
-    const [isLoading, setIsLoading] = useState(true);
+    const [searchParams] = useSearchParams();
+    const pageId = searchParams.get('page');
+
     const [currentCategory, setCurrentCategory] = useState({});
     const [subcategories, setSubcategories] = useState([]);
     const [siblingCategories, setSiblingCategories] = useState([]);
     const [productsData, setProductsData] = useState([]);
-    const [alert, setAlert] = useContext(AlertStateContext);
     const scroll = useRef();
+    const [subnav, setSubnav] = useState(false);
+
+    const showSubnav = () => setSubnav(!subnav);
 
     useEffect(() => {
         fetchCategoriesData();
         fetchProductsData();
-        setIsLoading(false);
 
         if (scroll.current) {
             scroll.current.scrollIntoView({ behavior: 'smooth' });
@@ -32,19 +35,19 @@ export function ProductsPanel (){
 
     const fetchCategoriesData = async () => {
             try {
-                const response = await axios.get(`https://localhost:7247/api/Warehouse/GetPartCategoryStructure?id=${categoryId}`);
+                const response = await axios.get(`${BACK_END_URL}/Warehouse/GetPartCategoryStructure?id=${categoryId}`);
                 prepareCategories(response.data);
             } catch (error) {
-                Error();
+                console.log(error)
             }
     };
     const fetchProductsData = async () => {
         const body = {"categoryId": categoryId, "page": pageId, "pageSize": 10}
         try {
-            const response = await axios.post(`https://localhost:7247/api/Warehouse/GetPartsPage`, body);
+            const response = await axios.post(`${BACK_END_URL}/Warehouse/GetPartsPage`, body);
             setProductsData(response.data);
         } catch (error) {
-            Error();
+            console.log(error)
         }
     };
     const prepareCategories=(data)=>{
@@ -53,47 +56,36 @@ export function ProductsPanel (){
         setSiblingCategories(data.siblingCategories);
     };
 
-    const Error=()=>{
-        setAlert({color:'danger', text: 'Błąd podczas pobierania danych, proszę odświeżyć stronę'})
-        setTimeout(() => {
-            setAlert();
-        }, 3000);
-    }
-
-    if(isLoading){
-        return(<p>ladowanie</p>)
-    }
-
     return (
         <>
-                <div ref={scroll}></div>
-                <div className="container m-0 p-0">
-                    <Header currentCategory={currentCategory} itemsCount={productsData.itemCount}/>
-                    <div className="row">
-                        <div className="col-3">
-                            <SideBar listFunction={<CategoriesList categories={subcategories}/>}>Podkategorie</SideBar>
-                        </div>
-                        <div className="col-9 bg-white p-4" >
-                            {productsData.itemCount < 1 ?
-                                <div className="row text-center">
-                                    <h2>Brak produktów w kategorii</h2>
-                                </div>
-                            :
-                                <>
-                                    <div className="row d-inline float-end mb-3">
-                                        <PaginationSmall pageId={pageId} pageCount={productsData.pageCount} path={`/warehouse/${categoryId}`}/>
-                                    </div>
-                                    <div className="row d-inline">
-                                        <ProductsList productsData={productsData.items}/>
-                                    </div>
-                                    <div className="row d-inline float-end mt-2">
-                                        <Pagination pageId={pageId} pageCount={productsData.pageCount} path={`/warehouse/${categoryId}`}/>
-                                    </div>
-                                </>
-                            }
-                        </div>
+            <div ref={scroll}></div>
+            <Container>
+                <Sidebar>
+                    <a className={styles.SidebarLink} onClick={showSubnav}>
+                        <span className={styles.SidebarLabel}>Podkategorie</span>
+                    </a>
+                    {subnav && subcategories.map((subcategory) => (
+                        <Link to={`/warehouse/${subcategory.id}?page=1`} key={subcategory.id} className={styles.DropdownLink}>
+                            <span className={styles.SidebarLabel}>{subcategory.name}</span>
+                        </Link>
+                        ))
+
+                    }
+                </Sidebar>
+                <MainContent>
+                    <div className={styles.toolbox}>
+                        <button>Dodaj</button>
+                        <button>Usuń</button>
                     </div>
-                </div>
+
+                    <PaginationSmall pageId={pageId} pageCount={productsData.pageCount} path={`/warehouse/${categoryId}`}/>
+
+                    <ProductsList productsData={productsData.items}/>
+
+                    <Pagination pageId={pageId} pageCount={productsData.pageCount} path={`/warehouse/${categoryId}`}/>
+
+                </MainContent>
+            </Container>
         </>
     );
 }
