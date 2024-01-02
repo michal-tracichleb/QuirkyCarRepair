43f371e29b32f3ebdb44e9d434b935a5b9ef2805
Oskar <oskar0201@onet.pl>
2024-01-02 18:11:01 +0100

    refactoring komponentu app

--- quirky-car-repair/src/App.jsx
+++ quirky-car-repair/src/App.jsx
@@ -1,51 +1,7 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min";
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import {NavBar} from "./components/NavBar/NavBar.jsx";
-import {LoginModal} from "./components/LoginModal/LoginModal.jsx";
-import {useEffect, useState} from "react";
-import {Warehouse} from "./components/Warehouse/Warehouse.jsx";
-import {ProductsPanel} from "./components/Warehouse/ProductsPanel/ProductsPanel.jsx";
-import {CategoriesPanel} from "./components/Warehouse/CategoriesPanel/CategoriesPanel.jsx";
-import {AlertStateContext} from "./context/AlertStateContext.js";
-import {Alert} from "./components/Alert/Alert.jsx";
+import {Layout} from "./components/Layout/Layout.jsx";
 
 export default function App() {
-    const [userIsLogged, setUserIsLogged] = useState(false);
-    const [userData, setUserData] = useState({});
-    const [alert, setAlert] = useState();
-
-    useEffect(()=>{
-        const sessionUserData = sessionStorage.user;
-
-        if(sessionUserData){
-            setUserData(JSON.parse(sessionUserData));
-            setUserIsLogged(true);
-        }
-    },[userIsLogged]);
-
     return (
-        <AlertStateContext.Provider value={[alert, setAlert]}>
-            {alert && <Alert color={alert.color}>{alert.text}</Alert>}
-            <Router>
-                <NavBar userData={userData} userIsLogged={userIsLogged} setUserIsLogged={setUserIsLogged}/>
-                {!userIsLogged && <LoginModal setUserIsLogged={setUserIsLogged}/>}
-                <Routes>
-                    <Route path="/"/>
-                    <Route
-                        path="/warehouse"
-                        element={<Warehouse/>}
-                    >
-                        <Route index element={<CategoriesPanel/>}/>
-                        <Route path=":categoryId/page/:pageId" element={<ProductsPanel/>}/>
-                    </Route>
-                    <Route path="/serwis"/>
-                    <Route path="/about"/>
-                    <Route path="/kontakt"/>
-                    <Route path="/user_profile"/>
-                </Routes>
-            </Router>
-        </AlertStateContext.Provider>
-
+        <Layout/>
     );
 }
