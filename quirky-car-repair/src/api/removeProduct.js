59b26f5ca5016f59400dcc83ee1eb44878540e05
Oskar <oskar0201@onet.pl>
2024-01-04 17:58:28 +0100

    funkcje API: pobranie kategorii, usunięcie produktu, eydcja produktu oraz stworzenie nowego produktu

---
+++ quirky-car-repair/src/api/removeProduct.js
@@ -0,0 +1,17 @@
+import {BACK_END_URL} from "../constans/backEndUrl.js";
+import axios from "axios";
+
+export async function removeProduct(id) {
+    const user = sessionStorage["user"] ? JSON.parse(sessionStorage["user"]) : [];
+    try {
+        await axios.delete(`${BACK_END_URL}/Warehouse/Part/${id}`, {
+            headers: {
+                Authorization: `Bearer ${user.token}`,
+            },
+        });
+        return { success: true, message: 'Usunięto produkt' };
+    } catch (error) {
+        console.error('Błąd podczas usuwania danych:', error.response.status);
+        return { success: false, message: 'Błąd podczas usuwania' };
+    }
+}
