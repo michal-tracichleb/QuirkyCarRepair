398c805d97f417c22c4c45abb4ec54dbacb805da
Oskar <oskar0201@onet.pl>
2024-01-04 18:06:22 +0100

    handler usuwania produktu

--- quirky-car-repair/src/components/Warehouse/ProductsPanel/ProductsPanel.jsx
+++ quirky-car-repair/src/components/Warehouse/ProductsPanel/ProductsPanel.jsx
@@ -1,6 +1,6 @@
 import styles from "./ProductsPanel.module.css"
 import {Link, useParams, useSearchParams} from "react-router-dom";
-import {useEffect, useState, useRef} from "react";
+import {useEffect, useState, useRef, useContext} from "react";
 import axios from "axios";
 import {ProductsList} from "../ProductsList/ProductsList.jsx";
 import {Pagination} from "../../Pagination/Pagination.jsx";
@@ -9,11 +9,19 @@ import {Container} from "../../ContentLayout/Container/Container.jsx";
 import {Sidebar} from "../../ContentLayout/Sidebar/Sidebar.jsx";
 import {MainContent} from "../../ContentLayout/MainContent/MainContent.jsx";
 import {BACK_END_URL} from "../../../constans/backEndUrl.js";
+import {ProductManageLink} from "../../ProductManageLink/ProductManageLink.jsx";
+import {UserStateContext} from "../../../context/UserStateContext.js";
+import {AlertStateContext} from "../../../context/AlertStateContext.js";
+import {removeProduct} from "../../../api/removeProduct.js";
 
 export function ProductsPanel (){
     const { categoryId } = useParams();
     const [searchParams] = useSearchParams();
     const pageId = searchParams.get('page');
+
+    const [userData] = useContext(UserStateContext);
+    const [,setAlert] = useContext(AlertStateContext)
+    const managementPermissions = userData.role.toLocaleLowerCase() === ('admin' || 'storekeeper');
 
     const [currentCategory, setCurrentCategory] = useState({});
     const [subcategories, setSubcategories] = useState([]);
@@ -56,6 +64,26 @@ export function ProductsPanel (){
         setSiblingCategories(data.siblingCategories);
     };
 
+    const HandleRemoveProduct = async  (id) =>{
+        const userConfirmed = window.confirm('Czy na pewno chcesz usunąć ten produkt?');
+
+        if(userConfirmed) {
+            const response = await removeProduct(id);
+            if(response.success){
+                setAlert({text: response.message, color: 'success'});
+
+                const updatedProducts = [...productsData.items];
+                const filteredProducts = updatedProducts.filter((product) => product.id !== id);
+                setProductsData(prevValues => ({ ...prevValues, items: filteredProducts }));
+            }else{
+                setAlert({text: response.message, color: 'warning'});
+            }
+            setTimeout(() => {
+                setAlert();
+            }, 3000);
+        }
+    }
+
     return (
         <>
             <div ref={scroll}></div>
@@ -73,14 +101,15 @@ export function ProductsPanel (){
                     }
                 </Sidebar>
                 <MainContent>
-                    <div className={styles.toolbox}>
-                        <button>Dodaj</button>
-                        <button>Usuń</button>
-                    </div>
+                    {managementPermissions &&
+                        <div className={styles.toolbox}>
+                            <ProductManageLink to="/warehouse/product/manage">Dodaj</ProductManageLink>
+                        </div>
+                    }
 
                     <PaginationSmall pageId={pageId} pageCount={productsData.pageCount} path={`/warehouse/${categoryId}`}/>
 
-                    <ProductsList productsData={productsData.items}/>
+                    <ProductsList productsData={productsData.items} removeAction={HandleRemoveProduct}/>
 
                     <Pagination pageId={pageId} pageCount={productsData.pageCount} path={`/warehouse/${categoryId}`}/>
 
