616bdade7aef8189be373f6214b381be350c2f69
Michał Tracichleb <111907641+michal-tracichleb@users.noreply.github.com>
2024-01-10 22:36:21 +0100

    DateStartRepair

--- QuirkyCarRepairApi/QuirkyCarRepair.DAL/Migrations/20240110213546_Add_DateStartRepair_ServiceOrder.Designer.cs
+++ QuirkyCarRepairApi/QuirkyCarRepair.DAL/Migrations/20240110213546_Add_DateStartRepair_ServiceOrder.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using QuirkyCarRepair.DAL;
 
@@ -11,9 +12,11 @@ using QuirkyCarRepair.DAL;
 namespace QuirkyCarRepair.DAL.Migrations
 {
     [DbContext(typeof(QuirkyCarRepairContext))]
-    partial class QuirkyCarRepairContextModelSnapshot : ModelSnapshot
+    [Migration("20240110213546_Add_DateStartRepair_ServiceOrder")]
+    partial class Add_DateStartRepair_ServiceOrder
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
