bfb21fc06958bd79999ecfd070bf86cd21eda13e
Oskar <oskar0201@onet.pl>
2024-01-11 15:55:34 +0100

    const vehicle brands

---
+++ quirky-car-repair/src/constans/vehicleBrands.js
@@ -0,0 +1,191 @@
+export const vehicleBrands = [
+    {
+        "name": "Acura"
+    },
+    {
+        "name": "Alfa Romeo"
+    },
+    {
+        "name": "Aston Martin"
+    },
+    {
+        "name": "Audi"
+    },
+    {
+        "name": "Bentley"
+    },
+    {
+        "name": "BMW"
+    },
+    {
+        "name": "Bugatti"
+    },
+    {
+        "name": "Cadillac"
+    },
+    {
+        "name": "Chevrolet"
+    },
+    {
+        "name": "Chrysler"
+    },
+    {
+        "name": "Citroën"
+    },
+    {
+        "name": "Corvette"
+    },
+    {
+        "name": "Dacia"
+    },
+    {
+        "name": "Dodge"
+    },
+    {
+        "name": "Ferrari"
+    },
+    {
+        "name": "Fiat"
+    },
+    {
+        "name": "Ford"
+    },
+    {
+        "name": "Honda"
+    },
+    {
+        "name": "Hummer"
+    },
+    {
+        "name": "Hyundai"
+    },
+    {
+        "name": "Infiniti"
+    },
+    {
+        "name": "Isuzu"
+    },
+    {
+        "name": "Jaguar"
+    },
+    {
+        "name": "Jeep"
+    },
+    {
+        "name": "Kia"
+    },
+    {
+        "name": "Koenigsegg"
+    },
+    {
+        "name": "Lamborghini"
+    },
+    {
+        "name": "Lancia"
+    },
+    {
+        "name": "Land Rover"
+    },
+    {
+        "name": "Lexus"
+    },
+    {
+        "name": "Lincoln"
+    },
+    {
+        "name": "Lotus"
+    },
+    {
+        "name": "Maruti Suzuki"
+    },
+    {
+        "name": "Maserati"
+    },
+    {
+        "name": "Maybach"
+    },
+    {
+        "name": "Mazda"
+    },
+    {
+        "name": "Mclaren"
+    },
+    {
+        "name": "Mercedes"
+    },
+    {
+        "name": "Mini"
+    },
+    {
+        "name": "Mitsubishi"
+    },
+    {
+        "name": "Nissan"
+    },
+    {
+        "name": "Opel"
+    },
+    {
+        "name": "Pagani"
+    },
+    {
+        "name": "Peugeot"
+    },
+    {
+        "name": "Porsche"
+    },
+    {
+        "name": "Renault"
+    },
+    {
+        "name": "Reva"
+    },
+    {
+        "name": "Rimac"
+    },
+    {
+        "name": "Rolls Royce"
+    },
+    {
+        "name": "Saab"
+    },
+    {
+        "name": "Scania"
+    },
+    {
+        "name": "Seat"
+    },
+    {
+        "name": "Shelby"
+    },
+    {
+        "name": "Skoda"
+    },
+    {
+        "name": "Smart"
+    },
+    {
+        "name": "Ssangyong"
+    },
+    {
+        "name": "Subaru"
+    },
+    {
+        "name": "Suzuki"
+    },
+    {
+        "name": "Tatra"
+    },
+    {
+        "name": "Tesla"
+    },
+    {
+        "name": "Toyota"
+    },
+    {
+        "name": "Volkswagen"
+    },
+    {
+        "name": "Volvo"
+    }
+]
