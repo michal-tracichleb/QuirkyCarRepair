a2be157dee1a3281a35050b2b73234b722c5cc4e
Oskar <oskar0201@onet.pl>
2024-01-12 20:26:37 +0100

    add user validation

--- quirky-car-repair/src/components/IconMenu/IconMenu.jsx
+++ quirky-car-repair/src/components/IconMenu/IconMenu.jsx
@@ -1,16 +1,21 @@
 import styles from "./IconMenu.module.css"
 import {Account} from "./account/Account.jsx";
 import {Cart} from "./cart/Cart.jsx";
+import {useContext} from "react";
+import {UserStateContext} from "../../context/UserStateContext.js";
 
 export function IconMenu(){
+    const [user] = useContext(UserStateContext);
     return(
         <ul className={styles.iconMenu}>
             <li>
                 <Account/>
             </li>
-            <li>
-                <Cart/>
-            </li>
+            {user && user.token &&
+                <li>
+                    <Cart/>
+                </li>
+            }
         </ul>
 
     )
