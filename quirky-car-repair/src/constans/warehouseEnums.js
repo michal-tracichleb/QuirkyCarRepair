3ec604276a483fce2b9c06cfcdf2f1c2d1d41a2b
Oskar <oskar0201@onet.pl>
2024-01-12 18:22:51 +0100

    new enums for service and warehouse*

---
+++ quirky-car-repair/src/constans/warehouseEnums.js
@@ -0,0 +1,19 @@
+export const orderStatus = {
+    Pending: 0,
+    Canceled: 1,
+    ArrangeOrder: 2,
+    ReadyForPickup: 3,
+    OrderCompleted: 4,
+    Return: 5,
+    AcceptedReturn: 6,
+    Complaint: 7,
+    Ready: 8
+};
+export const orderType = {
+    WW: 0,
+    WZ: 1,
+    ZZ: 2,
+    ZW: 3,
+    R: 4,
+    D: 5
+};
